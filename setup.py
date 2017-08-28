0668670320e2be84c8e7060125f351254bab2a02
Christopher Thorne <libcthorne@gmail.com>
2017-08-28 16:39:23 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scotchwsgi',
-    version='0.1.3',
+    version='0.1.4',
     author='Christopher Thorne',
     author_email='libcthorne@gmail.com',
     url='https://cthorne.me',
