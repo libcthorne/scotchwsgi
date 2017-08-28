35661b0a9cb5ea57001f6eade445a9c66de44da5
Christopher Thorne <libcthorne@gmail.com>
2017-08-28 08:34:25 +0100

    Update version from 0.1 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scotchwsgi',
-    version='0.1',
+    version='0.1.1',
     author='Christopher Thorne',
     author_email='libcthorne@gmail.com',
     url='https://cthorne.me',
