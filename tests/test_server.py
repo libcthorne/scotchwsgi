92e7046dd770c4910aab99f7faba420300b795ca
Christopher Thorne <libcthorne@gmail.com>
2017-09-20 20:19:30 +0100

    Refactor WSGIServer tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,73 +5,72 @@ from scotchwsgi.server import make_server
 
 TEST_HOST = "localhost"
 TEST_PORT = 0
+NUM_WORKERS = 10
 
-class TestServerWorkers(unittest.TestCase):
-    WORKER_COUNT = 10
-
-    @patch('scotchwsgi.server.multiprocessing.Process')
-    def test_workers_started(self, mock_process):
-        mock_process.side_effect = Mock
-
-        mock_app = Mock()
-        server = make_server(TEST_HOST, TEST_PORT, mock_app, num_workers=self.WORKER_COUNT)
-        server.start(blocking=False)
-
-        self.assertEqual(len(server.worker_processes), self.WORKER_COUNT)
-        for worker_process in server.worker_processes:
-            worker_process.start.assert_called_once()
-
-    @patch('scotchwsgi.server.multiprocessing.Process')
-    def test_workers_stopped_on_server_stop(self, mock_process):
+class BaseServerTestCase(unittest.TestCase):
+    def setUp(self):
         def get_mock_process(*args, **kwargs):
             return Mock(pid=0)
 
-        mock_process.side_effect = get_mock_process
+        self.mock_process = patch(
+            'scotchwsgi.server.multiprocessing.Process',
+            side_effect = get_mock_process
+        )
 
-        mock_app = Mock()
-        server = make_server(TEST_HOST, TEST_PORT, mock_app, num_workers=self.WORKER_COUNT)
+        self.mock_socket_instance = Mock(getsockname=lambda: (TEST_HOST, TEST_PORT))
+        self.mock_socket = patch(
+            'scotchwsgi.server.socket.socket',
+            return_value=self.mock_socket_instance,
+        )
+
+        self.mock_process.start()
+        self.mock_socket.start()
+
+        self.mock_app = Mock()
+
+    def tearDown(self):
+        self.mock_process.stop()
+        self.mock_socket.stop()
+
+class TestServerWorkers(BaseServerTestCase):
+    def test_workers_started(self):
+        server = make_server(TEST_HOST, TEST_PORT, self.mock_app, num_workers=NUM_WORKERS)
         server.start(blocking=False)
-        worker_processes = server.worker_processes
+
+        self.assertEqual(len(server.worker_processes), NUM_WORKERS)
+        for worker_process in server.worker_processes:
+            worker_process.start.assert_called_once()
+
+    def test_workers_stopped_on_server_stop(self):
+        server = make_server(TEST_HOST, TEST_PORT, self.mock_app, num_workers=NUM_WORKERS)
+        server.start(blocking=False)
+        worker_processes = server.worker_processes.copy()
         server.stop()
 
-        self.assertEqual(len(worker_processes), self.WORKER_COUNT)
         for worker_process in worker_processes:
             worker_process.terminate.assert_called_once()
 
-class TestServerSignalHandling(unittest.TestCase):
+class TestServerSignalHandling(BaseServerTestCase):
     def test_handle_signal_stops_server(self):
-        mock_app = Mock()
-        server = make_server(TEST_HOST, TEST_PORT, mock_app, num_workers=2)
+        server = make_server(TEST_HOST, TEST_PORT, self.mock_app, num_workers=NUM_WORKERS)
         server.start(blocking=False)
         self.assertEqual(server.alive, True)
         server.handle_signal(0, 0)
         self.assertEqual(server.alive, False)
 
-class TestServerSocketCreation(unittest.TestCase):
-    @patch('scotchwsgi.server.multiprocessing.Process')
-    @patch('scotchwsgi.server.socket.socket')
-    def test_no_backlog(self, mock_socket, mock_process):
-        mock_socket_instance = Mock(getsockname=lambda: (TEST_HOST, TEST_PORT))
-        mock_socket.return_value = mock_socket_instance
-
-        mock_app = Mock()
-        server = make_server(TEST_HOST, TEST_PORT, mock_app, num_workers=2)
+class TestServerSocketCreation(BaseServerTestCase):
+    def test_no_backlog(self):
+        server = make_server(TEST_HOST, TEST_PORT, self.mock_app, num_workers=NUM_WORKERS)
         server.start(blocking=False)
 
-        mock_socket_instance.listen.assert_called_with()
+        self.mock_socket_instance.listen.assert_called_with()
 
         server.stop()
 
-    @patch('scotchwsgi.server.multiprocessing.Process')
-    @patch('scotchwsgi.server.socket.socket')
-    def test_backlog(self, mock_socket, mock_process):
-        mock_socket_instance = Mock(getsockname=lambda: (TEST_HOST, TEST_PORT))
-        mock_socket.return_value = mock_socket_instance
-
-        mock_app = Mock()
-        server = make_server(TEST_HOST, TEST_PORT, mock_app, backlog=100, num_workers=2)
+    def test_backlog(self):
+        server = make_server(TEST_HOST, TEST_PORT, self.mock_app, backlog=100, num_workers=NUM_WORKERS)
         server.start(blocking=False)
 
-        mock_socket_instance.listen.assert_called_with(100)
+        self.mock_socket_instance.listen.assert_called_with(100)
 
         server.stop()
